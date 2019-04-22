37f277dc8d2e38741ac60eccd9c56c8b78cc26f2
peng.xing <peng.xing@172.19.104.156>
2019-04-22 11:20:16 +0800

    接收请求参数-逐个接收！

--- src/main/java/cn/ksdshpx/springmvc/controllers/annotation/MyController.java
+++ src/main/java/cn/ksdshpx/springmvc/controllers/annotation/MyController.java
@@ -1,5 +1,7 @@
 package cn.ksdshpx.springmvc.controllers.annotation;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,12 +10,21 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @RequestMapping("/springmvc")
 public class MyController {
-	@RequestMapping(value="/myhello",method= {RequestMethod.POST,RequestMethod.GET},params= {"name=zhangSan","age!=12"})
+	@RequestMapping(value = "/myhello", method = { RequestMethod.POST, RequestMethod.GET }, params = { "name=zhangSan",
+			"age!=12" })
 	public ModelAndView doHello() {
 		ModelAndView mv = new ModelAndView();
 		mv.addObject("name", "zhangSan");
-		mv.addObject("age","12");
+		mv.addObject("age", "12");
 		mv.setViewName("success");
 		return mv;
+	}
+
+	@RequestMapping("/testReceiveParam")
+	public String testReceiveParam(HttpServletRequest request, String name, Integer age) {
+		System.out.println(name + ":" + age);
+		request.setAttribute("name", name);
+		request.setAttribute("age", age);
+		return "success";
 	}
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -7,8 +7,13 @@
 <title>index.jsp</title>
 </head>
 <body>
-	<a href="some.do">发起some.do的请求</a>
-	<a href="springmvc/myhello">发起myhello的请求</a>
+	<a href="some.do">发起some.do的请求</a><br/>
+	<a href="springmvc/myhello">发起myhello的请求</a><br/>
 	<!-- <img alt="图片不能加载" src="images/ppp.png"/> -->
+	<form action="springmvc/testReceiveParam">
+		name:<input type="text" name="name"/><br/>
+		age:<input type="text" name="age"/><br/>
+		<input type="submit" value="Submit"/>
+	</form>
 </body>
 </html>
